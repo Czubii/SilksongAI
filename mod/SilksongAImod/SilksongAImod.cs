e430ff8dc06f16f09148e97090bc682963ef6bb4
Patryk Januś <patryk.jap@gmail.com>
2026-02-16 22:57:57 +0100

    moved the data get functions to utils.cs

--- mod/SilksongAImod/SilksongAImod.cs
+++ mod/SilksongAImod/SilksongAImod.cs
@@ -9,31 +9,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using SilksongAI;
 
-[BepInPlugin("com.czubii.SilksongAImod", "Silksong AI mod", "1.0.0 ")]
 
+public struct EnemyData
+{
+
+    public Vector3 pos;
+    public Vector2 vel;
+    public int hp;
+    public PlayMakerFSM fsm;
+
+}
+
+public struct HeroData
+{
+    Vector3 pos;
+    Vector3 vel;
+    int hp;
+}
+
+
+[BepInPlugin("com.czubii.SilksongAImod", "Silksong AI mod", "1.0.0 ")]
 public class SilksongAImod : BaseUnityPlugin
 {
     private ModGUI gui;
-    internal static ManualLogSource Log;
+    public static ManualLogSource Log;
 
     public static bool GodModeEnabled { get; set; } = false;
 
-    private struct EnemyData
-    {
-
-        public Vector3 pos;
-        public Vector2 vel;
-        public int hp;
-        public PlayMakerFSM fsm;
-
-    }
-
-    private struct HeroData
-    {
-        Vector3 pos;
-        Vector3 vel;
-        int hp;
-    }
 
 private void Awake()
     {
@@ -53,78 +55,7 @@ private void Awake()
         SceneManager.sceneLoaded -= TeleportUtils.OnSceneLoaded;
     }
 
-    private EnemyData? getEnemyData(string enemyName)
-    {
-        var enemy = GameObject.Find(enemyName);
-        if (enemy == null) return null;
-
-        var hm = enemy.GetComponent<HealthManager>();
-        if (hm == null) return null;
-
-        var fsm = enemy.GetComponent<PlayMakerFSM>();
-        if (fsm == null) return null;
-
-        var rigidbody = enemy.GetComponent<Rigidbody2D>();
-        if(rigidbody == null) return null;
-
-        EnemyData output = new EnemyData();
-
-        output.pos = enemy.transform.position;
-        output.vel = rigidbody.linearVelocity;
-        output.hp = hm.hp;
-        output.fsm = fsm;
-
-
-        return output;
-    }
-
-    private Vector3 getEnemyPosition(string enemyName)
-    {
-        var enemy = GameObject.Find(enemyName);
-        if (enemy != null)
-        {
-            return enemy.transform.position;
-        }
-        return Vector3.zero;
-    }
-
-
-    private void debugPrintGameObjectComponents(string gameObjectName)
-    {
-        var obj = GameObject.Find(gameObjectName);
-        if (obj == null) return;
-        int numComponentes = obj.GetComponentCount();
-        Log.LogInfo($"Num components: {numComponentes}");
-        for (int i = 0; i < numComponentes; i++)
-            Log.LogInfo(obj.GetComponentAtIndex(i));
-    }
-
-   private PlayMakerFSM[] getEnemyFSM(string enemyName)
-    {
-        var enemy = GameObject.Find(enemyName);
-        if (enemy != null)
-        {
-            var fsms = enemy.GetComponents<PlayMakerFSM>();
-            return fsms;
-        }
-
-        return null;
-        
-    }
-
-    private int getEnemyHP(string enemyName)
-    {
-        int hp = -1;
-
-        var enemy = GameObject.Find(enemyName);
-        if (enemy != null)
-        {
-            var hm = enemy.GetComponent<HealthManager>();
-            hp = hm.hp;
-        }
-
-        return hp;
-    }
+  
 
     
 
--- mod/SilksongAImod/Utils.cs
+++ mod/SilksongAImod/Utils.cs
@@ -34,4 +34,81 @@ namespace SilksongAI
             GameManager.instance.cameraCtrl.PositionToHeroInstant(false);
         }
     }
+
+    public static class GetDataUtils
+    {
+        private static EnemyData? getEnemyData(string enemyName)
+        {
+            var enemy = GameObject.Find(enemyName);
+            if (enemy == null) return null;
+
+            var hm = enemy.GetComponent<HealthManager>();
+            if (hm == null) return null;
+
+            var fsm = enemy.GetComponent<PlayMakerFSM>();
+            if (fsm == null) return null;
+
+            var rigidbody = enemy.GetComponent<Rigidbody2D>();
+            if (rigidbody == null) return null;
+
+            EnemyData output = new EnemyData();
+
+            output.pos = enemy.transform.position;
+            output.vel = rigidbody.linearVelocity;
+            output.hp = hm.hp;
+            output.fsm = fsm;
+
+
+            return output;
+        }
+
+        private static Vector3 GetEnemyPosition(string enemyName)
+        {
+            var enemy = GameObject.Find(enemyName);
+            if (enemy != null)
+            {
+                return enemy.transform.position;
+            }
+            return Vector3.zero;
+        }
+
+
+        private static void GetGameObjectComponents(string gameObjectName)
+        {
+            var obj = GameObject.Find(gameObjectName);
+            if (obj == null) return;
+            int numComponentes = obj.GetComponentCount();
+            SilksongAImod.Log.LogInfo($"Num components: {numComponentes}");
+            for (int i = 0; i < numComponentes; i++)
+                SilksongAImod.Log.LogInfo(obj.GetComponentAtIndex(i));
+        }
+
+        private static PlayMakerFSM[] GetEnemyFSM(string enemyName)
+        {
+            var enemy = GameObject.Find(enemyName);
+            if (enemy != null)
+            {
+                var fsms = enemy.GetComponents<PlayMakerFSM>();
+                return fsms;
+            }
+
+            return null;
+
+        }
+
+        private static int GetEnemyHP(string enemyName)
+        {
+            int hp = -1;
+
+            var enemy = GameObject.Find(enemyName);
+            if (enemy != null)
+            {
+                var hm = enemy.GetComponent<HealthManager>();
+                hp = hm.hp;
+            }
+
+            return hp;
+        }
+    }
+
 }
