aace9245bd5b7e4451989fecdfb4ad49a6460e13
Patryk Januś <patryk.jap@gmail.com>
2026-02-17 21:08:26 +0100

    added boss database with basic information

---
+++ mod/SilksongAImod/BossDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilksongAI
+{
+
+    public struct BossInfo
+    {
+        public string Name;
+        public string ArenaMap;
+        public Vector3 ArenaPosition;
+    }
+    public static class BossDatabase
+    {
+
+        public static readonly List<BossInfo> All = new List<BossInfo>
+        {
+            new BossInfo
+            {
+                Name = "Mossbone Mother",
+                ArenaMap = "Tut_03",
+                ArenaPosition = new Vector3(68f, 17.6f, 0)
+            },
+            new BossInfo
+            {
+                Name = "Bell Beast",
+                ArenaMap = "Tut_03",
+                ArenaPosition = new Vector3(68f, 17.6f, 0)
+            },
+            new BossInfo
+            {
+                Name = "Lace 1",
+                ArenaMap = "Tut_03",
+                ArenaPosition = new Vector3(68f, 17.6f, 0)
+            },
+
+        };
+    }
+}
