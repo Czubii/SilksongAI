cef3aab1a1f96669edcfe9ace0416714603cbbe8
Patryk Januś <patryk.jap@gmail.com>
2026-02-21 00:01:03 +0100

    added EnemyTracker class

---
+++ mod/SilksongAImod/EnemyTracker.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SilksongAI
+{
+    public class Enemy
+    {
+        public string Name; 
+        public GameObject GO;
+        public HealthManager HM;
+
+    }
+    public static class EnemyTracker
+    {
+        private static Dictionary<GameObject, Enemy> _enemies = 
+            new Dictionary<GameObject, Enemy>();
+
+        public static IEnumerable<Enemy> GetAll()
+        {
+            return _enemies.Values;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HealthManager), "OnEnable")]
+        private static void EnemyEnabled(HealthManager __instance)
+        {
+            _enemies[__instance.gameObject] = new Enemy
+            {
+                Name = __instance.name,
+                GO = __instance.gameObject,
+                HM = __instance
+            };
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HealthManager), "OnDisable")]
+        private static void EnemyDisabled(HealthManager __instance)
+        {
+            _enemies.Remove(__instance.gameObject);
+        }
+
+
+    }
+}
